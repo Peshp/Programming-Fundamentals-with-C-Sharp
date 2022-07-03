4912f85182010648e72877bfa12f05f4e7efe29d
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-07-04 00:33:00 +0300

    Add files via upload

---
+++ Objects and Classes/Lab/Big Factorial/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Numerics;
+
+namespace FundC194
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            BigInteger f = 1;
+            for (int i = 2; i < N; i++)
+            {
+                f *= i;
+            }
+
+            Console.WriteLine(f);
+        }
+    }
+}
