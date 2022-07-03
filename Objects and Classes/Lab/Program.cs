e6944fb5ae3246a3be48fda512ccb8a6aba6e8c7
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-07-04 00:30:37 +0300

    Add files via upload

---
+++ Objects and Classes/Lab/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FunC193
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+            Random randomize = new Random();
+            for (int i = 0; i < words.Length; i++)
+            {
+                int randomize2 = randomize.Next(words.Length);
+
+                var a = words[i];
+                var b = words[randomize2];
+
+                words[i] = b;
+                words[randomize2] = a;
+            }
+            Console.WriteLine(string.Join(Environment.NewLine, words));
+        }
+    }
+}
