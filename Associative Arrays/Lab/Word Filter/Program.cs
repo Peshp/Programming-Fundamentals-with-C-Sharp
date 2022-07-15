93a4b26165b0cd72bb2975b563486b3842c8014b
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-07-15 22:10:35 +0300

    Add files via upload

---
+++ Associative Arrays/Lab/Word Filter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace FundC211
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split().Where(w => w.Length % 2 == 0).ToArray();
+            for (int i = 0; i < words.Length; i++)
+            {
+                Console.WriteLine(words[i]);
+            }
+        }
+    }
+}
