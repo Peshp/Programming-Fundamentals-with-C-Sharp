eb58b6353dbd97cfebb3233600fc92c52c5d0a39
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-07-15 22:08:19 +0300

    Add files via upload

---
+++ Associative Arrays/Lab/Odd Occurrences/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FundC209
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+            var counts = new Dictionary<string, int>();
+            foreach (string word in words)
+            {
+                string inLower = word.ToLower();
+                if (counts.ContainsKey(inLower))
+                {
+                    counts[inLower]++;
+                }
+                else
+                { 
+                    counts.Add(inLower, 1);
+                }
+            }
+
+            foreach (var count in counts)
+            {
+                if (count.Value % 2 != 0)
+                {
+                    Console.Write(count.Key + " ");
+                }
+            }
+        }
+    }
+}
