8c6493fecb36648a4ea0a9010efb6bad32979807
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-07-18 12:04:29 +0300

    Add files via upload

---
+++ Associative Arrays/Exercise/Orders/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FundC215
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var products = new Dictionary<string, double>();
+            var quan = new Dictionary<string, int>();
+
+            string command = "";
+            while (command != "buy")
+            {
+                command = Console.ReadLine();
+                if (command == "buy")
+                {
+                    break;
+                }
+                string[] token = command.Split();
+
+                string product = token[0];
+                double price = double.Parse(token[1]);
+                int quantity = int.Parse(token[2]);
+
+                if (!products.ContainsKey(product))
+                {
+                    products[product] = 0;
+                    quan[product] = 0;
+                }
+
+                products[product] = price;
+                quan[product] += quantity;
+            }
+
+            foreach (var item in products)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value * quan[item.Key]:f2}");
+            }
+        }
+    }
+}
