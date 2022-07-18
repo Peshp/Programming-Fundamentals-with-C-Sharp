7351020cb7b225080284a0eec1278950ea358df5
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-07-18 12:03:35 +0300

    Add files via upload

---
+++ Associative Arrays/Exercise/A Miner Task/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FundC213
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var miner = new Dictionary<string, int>();
+            string mine = "";
+            int count = 0;
+
+            while (true)
+            {
+                mine = Console.ReadLine();
+                if (mine == "stop")
+                {
+                    break;
+                }
+                count = int.Parse(Console.ReadLine());
+
+                if (miner.ContainsKey(mine))
+                {
+                    miner[mine] += count;
+                }
+                else
+                {
+                    miner.Add(mine, count);
+                }
+            }
+
+            foreach (var mines in miner)
+            {
+                Console.WriteLine($"{mines.Key} -> {mines.Value}");
+            }
+        }
+    }
+}
