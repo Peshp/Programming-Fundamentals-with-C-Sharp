5e6bb873c6c8b93ad52813e1f81876d12d0b8262
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-07-20 14:02:16 +0300

    Add files via upload

---
+++ Associative Arrays/Exercise/Courses/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FundC217
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var courses = new Dictionary<string, List<string>>();
+            string command = "";
+
+            while (command != "end")
+            {
+                command = Console.ReadLine();
+                if (command == "end")
+                {
+                    break;
+                }
+                string[] token = command.Split(" : ");
+                string course = token[0];
+                string name = token[1];
+
+                if (!courses.ContainsKey(course))
+                {
+                    courses.Add(course, new List<string>());
+                }
+                courses[course].Add(name);
+            }
+
+            foreach (var course in courses)
+            {
+                Console.WriteLine($"{course.Key}: {course.Value.Count}");
+                foreach (var item in course.Value)
+                {
+                    Console.WriteLine($"-- {item}");
+                }
+            }
+        }
+    }
+}
