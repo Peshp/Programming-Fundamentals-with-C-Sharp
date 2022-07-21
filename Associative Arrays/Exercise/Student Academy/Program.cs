7671bd434667a98bcd1e980f1de92a231dcd457b
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-07-21 13:02:37 +0300

    Add files via upload

---
+++ Associative Arrays/Exercise/Student Academy/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FundC218
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var students = new Dictionary<string, List<double>>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+                double grade = double.Parse(Console.ReadLine());
+                if (!students.ContainsKey(name))
+                {
+                    students[name] = new List<double>();
+                }
+                students[name].Add(grade);
+            }
+
+            foreach (var student in students)
+            {
+                double avg = student.Value.Average();
+                if (avg >= 4.5)
+                {
+                    Console.WriteLine($"{student.Key} -> {avg:f2}");
+                }
+            }
+        }
+    }
+}
