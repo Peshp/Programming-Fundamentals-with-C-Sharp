f40bebafc429b2da901d08e1c67e13e0a03861db
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-07-22 12:35:49 +0300

    Add files via upload

---
+++ Text Processing/Lab/Repeat Strings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace FundC223
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] line = Console.ReadLine().Split();
+            string result = "";
+            foreach (var word in line)
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    result += word;
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
