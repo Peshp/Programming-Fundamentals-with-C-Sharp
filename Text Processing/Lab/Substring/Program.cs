28bb2bb7accb20f32994848242ccae8cf42c84f8
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-07-24 21:30:50 +0300

    Add files via upload

---
+++ Text Processing/Lab/Substring/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FundC224
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            string text = Console.ReadLine();
+
+            int txt = text.IndexOf(word);
+            while (txt != -1)
+            {
+                text = text.Remove(txt, word.Length);
+                txt = text.IndexOf(word);
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
