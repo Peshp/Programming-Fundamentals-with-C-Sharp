20a290725db693d22056e247166ab13c79d9da2b
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-07-24 21:31:48 +0300

    Add files via upload

---
+++ Text Processing/Lab/Text Filter/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FundC225
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] banned = Console.ReadLine().Split(", ");
+            string text = Console.ReadLine();
+            foreach (var ban in banned)
+            {
+                text = text.Replace(ban, new string('*', ban.Length));
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
