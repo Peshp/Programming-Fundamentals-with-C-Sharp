86ba8b33bb610c7820d27619b7cc8380582754c2
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-07-24 21:32:42 +0300

    Add files via upload

---
+++ Text Processing/Lab/Digits, Letters and Other/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using System.Xml;
+
+namespace FundC226
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder text = new StringBuilder();
+            text.Append(Console.ReadLine());
+
+            string nums = "";
+            string letter = "";
+            string chars = "";
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (Char.IsDigit(text[i]))
+                {
+                    nums += text[i];
+                }
+                else if (Char.IsLetter(text[i]))
+                {
+                    letter += text[i];
+                }
+                else
+                {
+                    chars += text[i];
+                }
+            }
+
+            Console.WriteLine(nums);
+            Console.WriteLine(letter);
+            Console.WriteLine(chars);
+        }
+    }
+}
