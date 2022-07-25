2d5ff4a7a70e96a8cd5e5a677934238f049726fc
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-07-25 13:02:27 +0300

    Add files via upload

---
+++ Text Processing/Exercise/Extract File/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace FundC229
+{
+    class Program
+    {
+        static void Main()
+        {
+            string[] file = Console.ReadLine().Split("\\");
+            string a = file[file.Length - 1];
+            string[] substract = a.Split(".");
+
+            Console.WriteLine($"File name: {substract[0]}");
+            Console.WriteLine($"File extension: {substract[1]}");
+        }
+    }
+}
