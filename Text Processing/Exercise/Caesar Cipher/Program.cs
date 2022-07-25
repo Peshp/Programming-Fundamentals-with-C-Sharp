56c647741e9320105faddbba9643ccab44962b6c
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-07-25 13:03:23 +0300

    Add files via upload

---
+++ Text Processing/Exercise/Caesar Cipher/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text;
+
+namespace FundC230
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string cipher = "";
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char letter = text[i];
+                letter += (char)3;
+                cipher += letter;
+            }
+            Console.WriteLine(cipher);
+        }
+    }
+}
