596f49999046b2599c7c5622706376f1ee3fa33d
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-07-25 13:01:36 +0300

    Add files via upload

---
+++ Text Processing/Exercise/Character Multiplier/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace FundC228
+{
+    class Program
+    {
+        static void Main()
+        {
+            List<string> words = Console.ReadLine().Split().ToList();
+            string first = words[0];
+            string second = words[1];
+
+            int min = Math.Min(first.Length, second.Length);
+            int max = Math.Max(first.Length, second.Length);
+            int sum = 0;
+
+            for (int i = 0; i < min; i++)
+            {
+                sum += (int)first[i] * (int)second[i];
+            }
+
+            if (first.Length != second.Length)
+            {
+                string inputLen = first.Length > second.Length ? inputLen = first : inputLen = second;
+                for (int i = min; i < max; i++)
+                {
+                    sum += inputLen[i];
+                }
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
