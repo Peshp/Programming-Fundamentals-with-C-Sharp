78328e800e42197705995d57f25f219cb201512e
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-07-26 16:08:34 +0300

    Add files via upload

---
+++ Text Processing/Exercise/Multiply Big Number/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+namespace FundC233
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            string big = Console.ReadLine();
+            int small = int.Parse(Console.ReadLine());
+            int remained = 0;
+
+            for (int i = big.Length - 1; i >= 0; i--)
+            {
+                if(small == 0)
+                {
+                    Console.WriteLine(0);
+                    return;
+                }
+
+                int currNum = int.Parse(big[i].ToString());
+                int result = currNum * small + remained;
+                sb.Append(result % 10);
+                remained = result / 10;
+            }
+            if(remained > 0)
+            {
+                sb.Append(remained);
+            }
+
+            char[] resultArray = sb.ToString().ToCharArray();
+            Array.Reverse(resultArray);
+            Console.WriteLine(resultArray);
+        }
+    }
+}
