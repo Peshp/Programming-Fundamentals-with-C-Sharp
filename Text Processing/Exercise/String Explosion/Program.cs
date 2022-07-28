3adc9cfad42f3255b7ef0b95629728ce26079471
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-07-28 13:39:46 +0300

    Add files via upload

---
+++ Text Processing/Exercise/String Explosion/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text;
+
+namespace FundC235
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+            string input = Console.ReadLine();
+
+            int explosion = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if(input[i] == '>')
+                {
+                    explosion += int.Parse(input[i + 1].ToString());
+                    sb.Append('>');
+                }
+                else if(explosion == 0)
+                {
+                    sb.Append(input[i]);
+                }
+                else
+                {
+                    explosion--;
+                }
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
