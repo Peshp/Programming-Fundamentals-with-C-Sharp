cc6bca82605d1fb71014f72c606f7ce9a88d70e0
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-07-30 08:59:47 +0300

    Add files via upload

---
+++ Regular Expressions/Exercise/SoftUni Bar Income/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace FundC239
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string command = "";
+            double total = 0;
+
+            while (command != "end of shift")
+            {
+                command = Console.ReadLine();
+                var regex = @"\%(?<name>[A-Z][a-z]+)\%[^[$%.]*\<(?<product>\w+)\>[^[$%.]*\|(?<count>\d+)\|[^[$%.]*?(?<price>\d+([.]\d+)?)\$";
+
+                MatchCollection valid = Regex.Matches(command, regex);
+                foreach  (Match item in valid)
+                {
+                    var name = item.Groups["name"].Value;
+                    var money = item.Groups["price"].Value;
+                    var quantity = item.Groups["count"].Value;
+                    var product = item.Groups["product"].Value;
+
+                    total += double.Parse(money) * double.Parse(quantity);
+
+                    Console.WriteLine($"{name}: {product} - {double.Parse(money) * int.Parse(quantity):f2}");                 
+                }
+            }
+            Console.WriteLine($"Total income; {total:f2}");
+        }
+    }
+}
