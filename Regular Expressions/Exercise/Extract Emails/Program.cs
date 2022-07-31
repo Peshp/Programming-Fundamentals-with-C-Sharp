9cf7192c25f352763811480b7c1bc8d4272ba999
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-07-31 22:49:15 +0300

    Add files via upload

---
+++ Regular Expressions/Exercise/Extract Emails/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace FundC242
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string regex = @"(^|\s)[A-Za-z0-9][\w*\-\.]*[A-Za-z0-9]*.[A-Za-z]@[A-Za-z]*([.-][A-Za-z]+)+\b";          
+            MatchCollection valid = Regex.Matches(text, regex);
+
+            foreach (Match item in valid)
+            {
+                Console.WriteLine(item.Value);
+            }         
+        }
+    }
+}
