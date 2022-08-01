01438b8f0f99933072b55bb307d0011031bd5a8f
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-01 07:17:36 +0300

    Add files via upload

---
+++ FinalExams/Final exam 1/The Imitation Game/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace _01Exam1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string message = Console.ReadLine();
+            string command = "";
+
+            while (command != "Decode")
+            {
+                command = Console.ReadLine();
+                string[] token = command.Split('|');
+                string type = token[0];
+
+                switch (type)
+                {
+                    case "Move":
+                        int length = int.Parse(token[1]);
+                        string moveMessage = message.Substring(0, length);
+
+                        message = message.Remove(0, length);
+                        message += moveMessage;
+                        break;
+                    case "Insert":
+                        int index = int.Parse(token[1]);
+                        string value = token[2];
+                        message = message.Insert(index, value);
+                        break;
+                    case "ChangeAll":
+                        string substring = token[1];
+                        string replace = token[2];
+
+                        message = message.Replace(substring, replace);
+                        break;
+                }
+            }
+            Console.WriteLine($"The decrypted message is: {string.Join(" ", message)}");
+        }
+    }
+}
