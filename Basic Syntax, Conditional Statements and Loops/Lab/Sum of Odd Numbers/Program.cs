ed53a7ed4d208d941125535843605848e5f59332
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-08 14:43:35 +0300

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops/Lab/Sum of Odd Numbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Fund8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int num = 0;
+            for (int i = 0; i < n; i++)
+            {
+                num = 1 + (i * 2);
+                sum += num;
+                Console.WriteLine(num);
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
