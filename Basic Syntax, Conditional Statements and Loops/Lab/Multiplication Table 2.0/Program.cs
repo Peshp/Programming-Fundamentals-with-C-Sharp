2c97d5d3fb852b594c885f27e63144b85623b6b9
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-08 14:45:59 +0300

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops/Lab/Multiplication Table 2.0/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FundC10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int secnum = int.Parse(Console.ReadLine());           
+            int mult = 0;
+            if(secnum > 10)
+            {
+                mult = num * secnum;
+                Console.WriteLine($"{num} X {secnum} = {mult}");
+            }
+            for (int i = secnum; i <= 10; i++)
+            {
+                mult = num * i;
+                Console.WriteLine($"{num} X {i} = {mult}");
+            }
+        }
+    }
+}
