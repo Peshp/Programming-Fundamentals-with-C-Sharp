61d2951f32f8ba91f7189df775545484e82a9808
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-09 12:33:16 +0300

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops/Exercise/Login/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace FundC17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            char[] stringArray = name.ToCharArray();
+            Array.Reverse(stringArray);
+            string reversename = new string(stringArray);
+            string pass = "";
+            int wrong = 0;
+
+            while (true)
+            {
+                pass = Console.ReadLine();              
+                wrong++;
+                if(wrong > 3)
+                {                   
+                    break;
+                }
+                else if(pass == reversename)
+                {
+                    break;
+                }
+                Console.WriteLine("Incorrect password. Try again.");
+            }
+            if(pass == reversename)
+            {
+                Console.WriteLine($"User {name} logged in.");
+            }
+            else
+            {
+                Console.WriteLine($"User {name} blocked!");
+            }
+        }
+    }
+}
