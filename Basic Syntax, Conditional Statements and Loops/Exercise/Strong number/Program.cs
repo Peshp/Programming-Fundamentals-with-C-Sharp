f7db03fe8c4a980fbd9308e267c3312769f3ef07
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-09 12:34:26 +0300

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops/Exercise/Strong number/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FundC18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int numcopy = num;
+            int sum = 0;
+
+            while (num > 0)
+            {
+                int factnum = 1;
+                int currnum = num % 10;
+                num /= 10;
+
+                for (int i = 2; i <= currnum; i++)
+                {
+                    factnum *= i;
+                }
+
+                sum += factnum;
+            }
+            Console.WriteLine(sum == numcopy ? "yes" : "no" );
+        }
+    }
+}
