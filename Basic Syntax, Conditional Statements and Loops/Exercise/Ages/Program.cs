eb39fa4bfe05f428fd3f5f3ee29ecef4ab82973a
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-09 12:26:49 +0300

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops/Exercise/Ages/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace FundC13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = int.Parse(Console.ReadLine());
+            if(age > 0 && age <= 2)
+            {
+                Console.WriteLine("baby");
+            }
+            else if(age > 2 && age <= 13)
+            {
+                Console.WriteLine("child");
+            }
+            else if (age > 13 && age <= 19)
+            {
+                Console.WriteLine("teenager");
+            }
+            else if (age > 19 && age <= 65)
+            {
+                Console.WriteLine("adult");
+            }
+            else if (age > 65)
+            {
+                Console.WriteLine("elder");
+            }
+            else
+            {
+                Console.WriteLine("inclusive");
+            }
+        }
+    }
+}
