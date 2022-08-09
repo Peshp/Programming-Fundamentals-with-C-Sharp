580802827f3fbd3d2966233b4f413bccc7cf7d8a
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-09 12:28:19 +0300

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops/Exercise/Division/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace FundC14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            int number = -1;
+            if(num % 10 == 0)
+            {
+                number = 10;
+            }
+            else if(num % 7 == 0)
+            {
+                number = 7;
+            }
+            else if (num % 6 == 0)
+            {
+                number = 6;
+            }
+            else if (num % 3 == 0)
+            {
+                number = 3;
+            }
+            else if (num % 2 == 0)
+            {
+                number = 2;
+            }
+
+            if(number == -1)
+            {
+                Console.WriteLine("Not divisible");
+            }
+            else
+            {
+                Console.WriteLine($"The number is divisible by {number}");
+            }
+        }
+    }
+}
