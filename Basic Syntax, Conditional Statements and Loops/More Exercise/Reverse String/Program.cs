5ce4940723c2a8dfaa759dc600dbea4f2227d9c0
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-12 22:41:22 +0300

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops/More Exercise/Reverse String/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FundC26
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            char[] stringArray = word.ToCharArray();
+            Array.Reverse(stringArray);
+            string reverse = new string(stringArray);
+
+            Console.WriteLine(reverse);
+        }
+    }
+}
