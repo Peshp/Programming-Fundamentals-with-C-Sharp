744f76688813da666d9feb342fd7f747ab869f7e
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-12 22:37:01 +0300

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops/Exercise/Orders/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FundC22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());          
+            double total = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                double pricecapsule = double.Parse(Console.ReadLine());
+                int days = int.Parse(Console.ReadLine());
+                int capsules = int.Parse(Console.ReadLine());
+
+                double price = days * capsules * pricecapsule;
+
+                Console.WriteLine($"The price for the coffee is: ${price:f2}");
+
+                total += price;
+            }
+            
+            Console.WriteLine($"Total: ${total:f2}");
+        }
+    }
+}
