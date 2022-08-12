b2a6a478661e24a34d2be60f0acb26df4f61ff71
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-12 22:40:41 +0300

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops/More Exercise/Gaming Store/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace FundC25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double balance = double.Parse(Console.ReadLine());
+            string gamename = Console.ReadLine();          
+            double totalPrice = 0;                  
+
+            while (gamename != "Game Time")
+            {
+                double price = 0;                             
+
+                switch(gamename)
+                {
+                    case "OutFall 4":
+                        price = 39.99;                       
+                        break;
+                    case "CS:OG":
+                        price = 15.99;                     
+                        break;
+                    case "Zplinter Zell":
+                        price = 19.99;         
+                        break;
+                    case "Honored 2":
+                        price = 59.99;
+                        break;
+                    case "RoverWatch":
+                        price = 29.99;                  
+                        break;
+                    case "RoverWatch Origins Edition":
+                        price = 39.99;                       
+                        break;
+                    default:
+                        Console.WriteLine("Not Found");                      
+                        break;
+                }
+                
+                if(balance - price < 0)
+                {
+                    Console.WriteLine("Too Expensive");
+                }
+                else 
+                {
+                    balance -= price;
+                    totalPrice += price;
+                    Console.WriteLine($"Bought {gamename}");
+                }
+                if(balance == 0)
+                {
+                    Console.WriteLine("Out of money!");
+                    return;
+                }
+                gamename = Console.ReadLine();
+            }                        
+            Console.WriteLine($"Total spent: ${totalPrice:f2}. Remaining: ${balance:f2}");                      
+        }
+    }
+}
