dad9949190c84b6d01218cca955e027a5abf30ea
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-12 22:38:46 +0300

    Add files via upload

---
+++ Basic Syntax, Conditional Statements and Loops/More Exercise/Sort Numbers/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace FundC23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+            int third = int.Parse(Console.ReadLine());                   
+
+            if(first > second && first > third)
+            {
+                if(second > third)
+                {
+                    Console.WriteLine($"{first}");
+                    Console.WriteLine($"{second}");
+                    Console.WriteLine($"{third}");
+                }
+                else
+                {
+                    Console.WriteLine($"{first}");
+                    Console.WriteLine($"{third}");
+                    Console.WriteLine($"{second}");
+                }
+            }
+            else if(second > first && second > third)
+            {
+                if(first > third)
+                {
+                    Console.WriteLine($"{second}");
+                    Console.WriteLine($"{first}");
+                    Console.WriteLine($"{third}");
+                }
+                else
+                {
+                    Console.WriteLine($"{second}");
+                    Console.WriteLine($"{third}");
+                    Console.WriteLine($"{first}");
+                }
+            }
+            else if(third > first && third > second)
+            {
+                if(first > second)
+                {
+                    Console.WriteLine($"{third}");
+                    Console.WriteLine($"{first}");
+                    Console.WriteLine($"{second}");
+                }
+                else
+                {
+                    Console.WriteLine($"{third}");
+                    Console.WriteLine($"{second}");
+                    Console.WriteLine($"{first}");
+                }
+            }
+        }
+    }
+}
