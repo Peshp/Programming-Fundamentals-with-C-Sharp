10f1037a4a71004e2a4e89fce63f5dd1b30a33ed
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-13 14:55:32 +0300

    Add files via upload

---
+++ Data Types and Variables/Lab/Reversed Chars/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FundC33
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char firstchar = char.Parse(Console.ReadLine());
+            char secondchar = char.Parse(Console.ReadLine());
+            char thirdchar = char.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{thirdchar} {secondchar} {firstchar}");
+        }
+    }
+}
