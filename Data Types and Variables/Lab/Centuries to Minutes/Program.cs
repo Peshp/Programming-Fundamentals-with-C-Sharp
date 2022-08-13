b0296fb63e3ed8008937527cdac192023ce0287d
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-13 14:54:09 +0300

    Add files via upload

---
+++ Data Types and Variables/Lab/Centuries to Minutes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace FundC31
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int cent = int.Parse(Console.ReadLine());
+            int years = cent * 100;
+            int days = (int)(years * 365.2422);
+            int hours = 24 * days;
+            int minutes = 60 * hours;
+
+            Console.WriteLine($"{cent} centuries = {years} years = {days} days = {hours} hours = {minutes} minutes");
+        }
+    }
+}
