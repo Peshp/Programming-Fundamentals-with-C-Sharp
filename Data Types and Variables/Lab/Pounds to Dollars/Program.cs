24cf53ca0c3a387116168bad82c4e12f99e678d5
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-13 14:52:36 +0300

    Add files via upload

---
+++ Data Types and Variables/Lab/Pounds to Dollars/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FundC29
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double pound = double.Parse(Console.ReadLine());
+            double dolar = pound * 1.31;
+            Console.WriteLine($"{dolar:f3}");
+        }
+    }
+}
