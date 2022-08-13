016f8708b7f27e188c3f405a956a2cc3877f7e50
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-13 14:51:54 +0300

    Add files via upload

---
+++ Data Types and Variables/Lab/Convert Meters to Kilometers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FundC28
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int m = int.Parse(Console.ReadLine());
+            float km = m / 1000.0f;
+            Console.WriteLine($"{km:f2}");
+        }
+    }
+}
