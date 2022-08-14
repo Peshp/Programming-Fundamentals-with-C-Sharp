7a1c470bccc228339e8d605b47e800be190f6e75
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-14 17:43:34 +0300

    Add files via upload

---
+++ Data Types and Variables/Lab/Lower or Upper/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FundC37
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char word = char.Parse(Console.ReadLine());
+            bool result = Char.IsUpper(word);
+            if(result == false)
+            {
+                Console.WriteLine("lower-case");
+            }
+            else
+            {
+                Console.WriteLine("upper-case");
+            }
+        }
+    }
+}
