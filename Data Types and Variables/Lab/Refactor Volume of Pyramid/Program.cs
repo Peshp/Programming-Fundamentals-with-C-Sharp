db45d254e47cda7041b12d389db65f5a1e2e0e33
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-14 17:44:28 +0300

    Add files via upload

---
+++ Data Types and Variables/Lab/Refactor Volume of Pyramid/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace FundC38
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double lenght, width, height = 0;           
+            lenght = double.Parse(Console.ReadLine());           
+            width = double.Parse(Console.ReadLine());           
+            height = double.Parse(Console.ReadLine());
+            double Volume = (lenght * width * height) / 3;
+            Console.Write($"Length: Width: Height: Pyramid Volume: {Volume:f2}");
+        }
+    }
+}
