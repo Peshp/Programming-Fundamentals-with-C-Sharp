a8adc96faeb9c71dddc2aa91ee2c8e4995998531
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-15 09:59:14 +0300

    Add files via upload

---
+++ Data Types and Variables/Exercise/Triples of Latin Letters/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace FundC45
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                char firstchar = (char)('a' + i);
+                for (int j = 0; j < n; j++)
+                {
+                    char secondchar = (char)('a' + j);
+                    for (int t = 0; t < n; t++)
+                    {
+                        char thirdchar = (char)('a' + t);
+                        Console.WriteLine($"{firstchar}{secondchar}{thirdchar}");
+                    }
+                }
+            }
+        }
+    }
+}
