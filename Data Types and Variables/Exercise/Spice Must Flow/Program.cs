b7fbfc8f251521fb5eb6c484e4086ee10bf0d0c1
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-15 10:03:04 +0300

    Add files via upload

---
+++ Data Types and Variables/Exercise/Spice Must Flow/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace FundC48
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int yield = int.Parse(Console.ReadLine());           
+            int days = 0;
+            int consumespice = 0;
+            if(yield < 100)
+            {
+                Console.WriteLine(days);
+                Console.WriteLine(consumespice);
+            }
+            else
+            {
+                while (yield >= 100)
+                {
+                    days++;
+                    consumespice += yield - 26;                                   
+                    yield -= 10;
+                }               
+                consumespice -= 26;                             
+                Console.WriteLine(days);
+                Console.WriteLine(consumespice);
+            }                                             
+        }
+    }
+}
