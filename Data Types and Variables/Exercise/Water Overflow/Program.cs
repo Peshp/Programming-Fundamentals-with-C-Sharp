c2caf3c6e72fb07cff9fca6bd1fbafece9287f77
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-15 10:00:50 +0300

    Add files via upload

---
+++ Data Types and Variables/Exercise/Water Overflow/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FundC46
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());           
+            int pourwater = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int quantities = int.Parse(Console.ReadLine());
+                pourwater += quantities;
+                if (pourwater > 255)
+                {
+                    pourwater -= quantities;
+                    Console.WriteLine("Insufficient capacity!");
+                }                              
+            }
+            Console.WriteLine(pourwater);
+        }
+    }
+}
