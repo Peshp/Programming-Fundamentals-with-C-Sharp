0367d4779c61d7d4f19b3935e21a2e3d7ceda362
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-15 09:55:33 +0300

    Add files via upload

---
+++ Data Types and Variables/Exercise/Elevator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace FundC42
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double n = double.Parse(Console.ReadLine());
+            double p = double.Parse(Console.ReadLine());
+
+            double courses = Math.Ceiling(n / p);
+            Console.WriteLine(courses);
+        }
+    }
+}
