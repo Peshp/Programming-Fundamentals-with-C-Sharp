f6cbb6cb942955f63120140070c1d76c60195767
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-15 10:01:51 +0300

    Add files via upload

---
+++ Data Types and Variables/Exercise/Beer Kegs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FundC47
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double volume = 0;
+            double biggestkeg = double.MinValue;
+            string biggest = "";
+            for (int i = 0; i < n; i++)
+            {
+                string kegmodel = Console.ReadLine();
+                var radius = double.Parse(Console.ReadLine());
+                var height = int.Parse(Console.ReadLine());
+
+                volume = Math.PI * radius * radius * height;
+
+                if(volume > biggestkeg)
+                {
+                    biggestkeg = volume;
+                    biggest = kegmodel;
+                }               
+            }
+            Console.WriteLine(biggest);
+        }
+    }
+}
