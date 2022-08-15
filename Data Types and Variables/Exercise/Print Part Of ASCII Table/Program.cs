8155e4afafd014f9028d4baf41e58056710f65fb
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-15 09:58:07 +0300

    Add files via upload

---
+++ Data Types and Variables/Exercise/Print Part Of ASCII Table/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FundC44
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+
+            for (int i = start; i < second; i++)
+            {
+                char curr = (char)i;
+                Console.WriteLine($"{curr} ");
+            }
+        }
+    }
+}
