91462032db7b0be6fd0b3c9f812325d2ccabd9f9
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-16 08:53:24 +0300

    Add files via upload

---
+++ Arrays/Lab/Day of Week/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FundC55
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int day = int.Parse(Console.ReadLine());
+            string[] daysWeek = { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+
+            if(day > 0 && day < 8)
+            {
+                Console.WriteLine(daysWeek[day - 1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
+        }
+    }
+}
