aab10c43f64c4960c2a2f8d33414a8ff85ee3bea
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-16 08:55:51 +0300

    Add files via upload

---
+++ Arrays/Lab/Reverse Array of Strings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FundC58
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+
+            Array.Reverse(words);
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                Console.Write(words[i] + " ");
+            }          
+        }
+    }
+}
