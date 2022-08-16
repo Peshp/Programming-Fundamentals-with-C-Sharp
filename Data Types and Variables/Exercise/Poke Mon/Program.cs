1b9e592f47d168dee01b0f920bf289cf470ead36
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-16 08:51:36 +0300

    Add files via upload

---
+++ Data Types and Variables/Exercise/Poke Mon/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FundC49
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine()); 
+            int M = int.Parse(Console.ReadLine()); 
+            sbyte Y = sbyte.Parse(Console.ReadLine());            
+            int pokedtargets = 0;
+            int originalvaluebytwo = N / 2;
+            while (N >= M)
+            {
+                N -= M;
+                pokedtargets++;               
+                if(N == originalvaluebytwo)
+                {
+                    if(Y == 0)
+                    {
+                        N *= 1;
+                    }
+                    else if(N / Y >= 1)
+                    {
+                        N /= Y;
+                    }
+                }
+            }
+            Console.WriteLine(N);
+            Console.WriteLine(pokedtargets);
+        }
+    }
+}
