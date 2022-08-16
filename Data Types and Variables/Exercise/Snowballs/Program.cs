dc62cb9264a7cfe6837375310fc5df5ef24055e3
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-16 08:52:13 +0300

    Add files via upload

---
+++ Data Types and Variables/Exercise/Snowballs/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Numerics;
+
+namespace FundC50
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            BigInteger highest = 0;
+            int highestsnow = 0;
+            int highesttime = 0;
+            int highestquality = 0;
+            for (int i = 0; i < N; i++)
+            {
+                int snowballSnow = int.Parse(Console.ReadLine());
+                int snowballTime = int.Parse(Console.ReadLine());
+                int snowballQuality = int.Parse(Console.ReadLine());
+
+                BigInteger snowballValue = BigInteger.Pow(snowballSnow / snowballTime, snowballQuality);
+                if(snowballValue > highest)
+                {
+                    highest = snowballValue;
+                    highestsnow = snowballSnow;
+                    highesttime = snowballTime;
+                    highestquality = snowballQuality;
+                }
+            }
+            Console.WriteLine($"{highestsnow} : {highesttime} = {highest} ({highestquality})");
+        }
+    }
+}
