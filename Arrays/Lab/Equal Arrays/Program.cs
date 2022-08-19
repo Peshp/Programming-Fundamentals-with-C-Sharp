21f27f79d06355be5905de0efd9e4243916b58dd
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-19 13:48:22 +0300

    Add files via upload

---
+++ Arrays/Lab/Equal Arrays/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace FundC61
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstNums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] secondNums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int sum = 0;
+
+            for (int i = 0; i < firstNums.Length; i++)
+            {
+                if(firstNums[i] != secondNums[i])
+                {                   
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    return;
+                }
+                else
+                {
+                    sum += firstNums[i];
+                }
+            }
+            Console.WriteLine($"Arrays are identical. Sum: {sum}");
+        }
+    }
+}
