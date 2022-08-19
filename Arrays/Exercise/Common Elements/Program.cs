bbf465bdcd80b92d18e777593c2f87062c11edcf
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-19 13:52:57 +0300

    Add files via upload

---
+++ Arrays/Exercise/Common Elements/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FundC64
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] first = Console.ReadLine().Split();
+            string[] second = Console.ReadLine().Split();
+            string common = "";
+      
+
+            for (int i = 0; i < first.Length; i++)
+            {
+                for (int j = 0; j < second.Length; j++)
+                {
+                    if(second[j] == first[i])
+                    {
+                        Console.Write(first[i] + " ");
+                    }
+                }               
+            }           
+        }
+    }
+}
