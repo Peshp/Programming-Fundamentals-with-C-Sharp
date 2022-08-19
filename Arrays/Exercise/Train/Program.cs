44e11d692c66da896d04a77a0fa5600e59cbd8af
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-19 13:51:50 +0300

    Add files via upload

---
+++ Arrays/Exercise/Train/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FundC63
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] people = new int[n];
+            int totalpeople = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                people[i] = int.Parse(Console.ReadLine());
+                totalpeople += people[i];
+            }
+
+            for (int i = 0; i < people.Length; i++)
+            {
+                Console.Write(people[i] + " ");
+            }
+            
+            Console.WriteLine();
+            Console.WriteLine(totalpeople);
+        }
+    }
+}
