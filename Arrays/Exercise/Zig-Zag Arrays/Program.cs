0d8deb3015522fa207322512401a6df797c0a361
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-19 13:53:49 +0300

    Add files via upload

---
+++ Arrays/Exercise/Zig-Zag Arrays/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace FundC65
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] even = new int[n];
+            int[] odd = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                int[] numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                if (i % 2 == 0)
+                {
+                    even[i] = numbers[0];
+                    odd[i] = numbers[1];
+                }
+                else
+                {
+                    even[i] = numbers[1];
+                    odd[i] = numbers[0];
+                }
+            }
+            Console.WriteLine(String.Join(" ", even));
+            Console.WriteLine(String.Join(" ", odd));
+        }
+    }
+}
