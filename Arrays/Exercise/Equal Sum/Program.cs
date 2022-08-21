0c129bb432e6123a88ad487cd5014e944b08e58c
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-21 14:40:08 +0300

    Add files via upload

---
+++ Arrays/Exercise/Equal Sum/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace FundC68
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int leftsum = 0;
+            int rightsum = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if(nums.Length == 1)
+                {
+                    Console.WriteLine(0);
+                    return;
+                }
+
+                leftsum = 0;
+                for (int j = i; j > 0; j--) // 1 2 3 3
+                {
+                    int leftnums = j - 1; // 1 2 
+                    if(j > 0) // 1 2 3
+                    {
+                        leftsum += nums[leftnums]; // +1 +2
+                    }        
+                }
+
+                rightsum = 0;
+                for (int t = i; t < nums.Length; t++) // 1 2 3 3
+                {
+                    int rightnums = t + 1; // 3 3
+                    if(t < nums.Length - 1) // 3
+                    {
+                        rightsum += nums[rightnums];
+                    }                 
+                }
+
+                if(rightsum == leftsum)
+                {
+                    Console.WriteLine(i);
+                    return;
+                }
+            }
+            Console.WriteLine("no");
+        }
+    }
+}
