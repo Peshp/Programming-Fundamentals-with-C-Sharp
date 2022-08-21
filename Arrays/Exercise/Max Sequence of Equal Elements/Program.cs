b19143e0b6f3308ad86ce63eb1a9e59211faa3d5
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-21 14:41:43 +0300

    Add files via upload

---
+++ Arrays/Exercise/Max Sequence of Equal Elements/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace FundC69
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int num = int.Parse(Console.ReadLine());
+            int secondnums = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                bool isSum = false;
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if(nums[i] + nums[j] == num)
+                    {
+                        isSum = true;
+                        secondnums = nums[j];
+                    }                  
+                }
+                if (isSum == true)
+                {
+                    Console.WriteLine($"{nums[i]} {secondnums}");
+                }
+
+            }
+        }
+    }
+}
