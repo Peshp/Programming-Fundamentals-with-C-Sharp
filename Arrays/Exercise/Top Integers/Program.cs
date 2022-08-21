e7eb778768e3301ce477233d6ada3cb55e4d7266
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-21 14:38:16 +0300

    Add files via upload

---
+++ Arrays/Exercise/Top Integers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace FundC67
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine().Split().Select(int.Parse).ToArray();          
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                bool top = true;
+                for (int j = i + 1; j < nums.Length; j++)
+                {
+                    if(nums[i] <= nums[j])
+                    {
+                        top = false;
+                    }
+                }
+
+                if(top)
+                {
+                    Console.Write(nums[i] + " ");
+                }
+            }
+        }
+    }
+}
