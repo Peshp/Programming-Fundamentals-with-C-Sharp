5df37e2d961704606ff409ee06bb1b0dd607d925
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-21 14:43:15 +0300

    Add files via upload

---
+++ Arrays/Exercise/Magic Sum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace FundC70
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine().Split();
+
+            string sequence = "";
+            int count = 0;
+            int currcount = 0;
+
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                if(arr[i] == arr[i + 1])
+                {
+                    currcount++;
+                    if(currcount > count)
+                    {
+                        count = currcount;
+                        sequence = arr[i];
+                    }
+                }
+                else
+                {
+                    currcount = 0;
+                }
+            }
+
+            for (int i = 0; i <= count; i++)
+            {
+                Console.Write(sequence + " ");
+            }
+        }
+    }
+}
