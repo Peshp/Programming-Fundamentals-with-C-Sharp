c82c46a51d8c232b9b953e364c351992b7cbaf72
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-22 17:54:53 +0300

    Add files via upload

---
+++ Methods/Lab/Calculate Rectangle Area/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FundC78
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double area = RectangleArea(a, b);
+            Console.WriteLine(area);
+        }
+        static double RectangleArea(double a, double b)
+        {
+            return a * b;          
+        }
+    }
+}
