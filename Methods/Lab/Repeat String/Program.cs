705028742a50b277b2be98f6e22ae058c19ff0a6
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-22 17:56:47 +0300

    Add files via upload

---
+++ Methods/Lab/Repeat String/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace FundC79
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            int n = int.Parse(Console.ReadLine());
+            string result = RepeatWord(word, n);
+            Console.Write(result);
+        }
+        private static string RepeatWord(string word, int n)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = 0; i < n; i++)
+            {
+                result.Append(word);
+            }
+            return result.ToString();
+        }
+    }
+}
