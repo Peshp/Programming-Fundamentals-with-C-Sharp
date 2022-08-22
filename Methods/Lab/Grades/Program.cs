d59bbbf268796b246fb8bc290ccb51636fb6e7a1
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-22 17:47:46 +0300

    Add files via upload

---
+++ Methods/Lab/Grades/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace FundC74
+{
+    class Program
+    {
+        static void Main()
+        {
+            grades(double.Parse(Console.ReadLine()));
+        }
+
+        static void grades(double grade)
+        {
+            if(grade < 3.00)
+            {
+                Console.WriteLine("Fail");
+            }          
+            else if(grade > 2.99 && grade < 3.50)
+            {
+                Console.WriteLine("Poor");
+            }
+            else if(grade > 3.49 && grade < 4.50)
+            {
+                Console.WriteLine("Good");
+            }
+            else if(grade > 4.49 && grade < 5.50)
+            {
+                Console.WriteLine("Very good");
+            }
+            else
+            {
+                Console.WriteLine("Excellent");
+            }
+        }
+    }
+}
