7d26537fe92f51abefe8d88beeee85c15c2de4f9
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-22 17:45:45 +0300

    Add files via upload

---
+++ Methods/Lab/Sign of Integer Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace FundC73
+{
+    class Program
+    {
+        static void Main()
+        {
+            Number(int.Parse(Console.ReadLine()));
+        }
+        static void Number(int num)
+        {
+            if (num > 0)
+            {
+                Console.WriteLine($"The number {num} is positive.");
+            }
+            else if (num < 0)
+            {
+                Console.WriteLine($"The number {num} is negative.");
+            }
+            else
+            {
+                Console.WriteLine($"The number {num} is zero.");
+            }
+        }
+    }
+}
