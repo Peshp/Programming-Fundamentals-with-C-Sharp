deb971c3b07946e554c300b8a10107f71846201c
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-23 23:37:57 +0300

    Add files via upload

---
+++ Methods/Lab/Math operations/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace FundC83
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            string @operator = Console.ReadLine();
+            int second = int.Parse(Console.ReadLine());
+
+            double result = Calculate(first, @operator, second);
+            Console.WriteLine(result);
+        }
+        private static double Calculate(int first, string @operator, int second)
+        {
+            double result = 0;
+            switch(@operator)
+            {
+                case "/":
+                    result = first / second;
+                    break;
+                case "*":
+                    result = first * second;
+                    break;
+                case "+":
+                    result = first + second;
+                    break;
+                case "-":
+                    result = first - second;
+                    break;
+            }
+            return result;
+        }
+    }
+}
