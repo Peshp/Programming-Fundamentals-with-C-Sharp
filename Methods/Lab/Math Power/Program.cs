0ef6267ea10bec207a0c00c92da60c285958b29f
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-23 23:35:50 +0300

    Add files via upload

---
+++ Methods/Lab/Math Power/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace FundC80
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double b = double.Parse(Console.ReadLine());
+            double result = FinalResult(a, b);
+            Console.WriteLine(result);
+        }
+        static double FinalResult(double a, double b)
+        {
+            return Math.Pow(a, b);
+        }
+    }
+}
