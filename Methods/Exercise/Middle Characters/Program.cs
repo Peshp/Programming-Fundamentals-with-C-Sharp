e143868dd4ad3e0552428c63bca3fdb1d1b0abf7
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-30 05:50:50 +0300

    Add files via upload

---
+++ Methods/Exercise/Middle Characters/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FundC89
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            string result = MiddleChar(word);
+            Console.WriteLine(result);
+        }
+        private static string MiddleChar(string word)
+        {
+            string result = "";
+
+            int l = 1 - word.Length % 2;
+            result = word.Substring(word.Length / 2 - l, 1 + l);
+            return result;
+        }
+    }
+}
