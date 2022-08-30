eafe917b703c5a03852c89c2ae00210f1cae55c9
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-30 05:49:52 +0300

    Add files via upload

---
+++ Methods/Exercise/Add and Subtract/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace FundC88
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int one = int.Parse(Console.ReadLine());
+            int two = int.Parse(Console.ReadLine());
+            int three = int.Parse(Console.ReadLine());
+            int res = Sum(one, two);
+            Console.WriteLine(res - three);
+        }
+        private static int Sum(int one, int two)
+        {
+            int res = one + two;
+            return res;
+        }
+    }
+}
