178fb215441bfc72d05c662d59fb0cdf07db1c66
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-30 05:46:13 +0300

    Add files via upload

---
+++ Methods/Exercise/Vowels Count/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FundC85
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentences = Console.ReadLine().ToLower();
+            int result = VowelsCount(sentences);
+            Console.WriteLine(result);
+        }
+        private static int VowelsCount(string sentences)
+        {
+            int count = 0;
+            for (int i = 0; i < sentences.Length; i++)
+            {
+                if(sentences[i] == 'a' || sentences[i] == 'e' || sentences[i] == 'i' || 
+                    sentences[i] == 'o' || sentences[i] == 'u')
+                {
+                    count++;
+                }
+
+            }
+            return count;
+        }
+    }
+}
