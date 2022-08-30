eb462b9a8dbaa8f5b1d751a4e13a53cfc87cd3fd
Anton Todorov <98225837+Peshp@users.noreply.github.com>
2022-08-30 05:45:07 +0300

    Add files via upload

---
+++ Methods/Exercise/Smallest of Three Numbers/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FundC84
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int one = int.Parse(Console.ReadLine());
+            int two = int.Parse(Console.ReadLine());
+            int three = int.Parse(Console.ReadLine());        
+            
+            int result = WhoisBiggest(one, two, three);
+            Console.WriteLine(result);
+        }
+        private static int WhoisBiggest(int one, int two, int three)
+        {
+            int min = 0;
+            if (one <= two && one <= three)
+                min = one;
+            if (two <= one && two <= three)
+                min = two;
+            if (three <= one && three <= two)
+                min = three;
+
+            return min;
+        }
+    }
+}
